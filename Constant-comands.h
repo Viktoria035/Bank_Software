4ca9a9c927791feea585d21e90dedd2fe6dd4b4c
Viktoria035 <115097521+Viktoria035@users.noreply.github.com>
2023-01-18 00:43:19 +0200

    Add files via upload

---
+++ Constant-comands.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+const string QUIT_COMMAND = "Q";
+const string LOGIN_COMMAND = "L";
+const string REGISTER_COMMAND = "R";
+const string CANCEL_COMMAND = "C";
+const string DEPOSIT_COMMAND = "D";
+const string LOGOUT_COMMAND = "L";
+const string TRANSFER_COMMAND = "T";
+const string WITHDRAW_COMMAND = "W";
+
+const string REGISTERED_SUCCESSFULLY_MESSAGE = "Registered successfully";
