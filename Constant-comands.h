6f9b570048d1a7d007f3053d581ab92c07002bd9
Viktoria035 <115097521+Viktoria035@users.noreply.github.com>
2023-01-19 22:01:30 +0200

    New commands added

--- Constant-comands.h
+++ Constant-comands.h
@@ -1,7 +1,22 @@
+/**
+*
+* Solution to course project # 9
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2022 / 2023
+*
+*@author Viktoria Koleva
+*@idnumber 5MI0600139 * @compiler VC
+*
+*<file with constant-commands declarations>
+*
+*/
 #pragma once
 #include <string>
 
 using namespace std;
+
+const int MIN = -10000;
 
 const string QUIT_COMMAND = "Q";
 const string LOGIN_COMMAND = "L";
@@ -11,5 +26,7 @@ const string DEPOSIT_COMMAND = "D";
 const string LOGOUT_COMMAND = "L";
 const string TRANSFER_COMMAND = "T";
 const string WITHDRAW_COMMAND = "W";
-
-const string REGISTERED_SUCCESSFULLY_MESSAGE = "Registered successfully";
+const string INVALID_USERNAME = "Invalid username.";
+const string INVALID_PASSWORD = "Invalid password.";
+const string EXISTING_USERNAME_ALREADY = "User with this name already exists.";
+const string REGISTERED_SUCCESSFULLY_MESSAGE = "Congratulations, you have just registered successfully";
