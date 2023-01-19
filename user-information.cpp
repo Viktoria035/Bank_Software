88c49caf9868690e8f64b569eeed9d7703a41cce
Viktoria035 <115097521+Viktoria035@users.noreply.github.com>
2023-01-19 22:03:29 +0200

    Update user-information.cpp

--- user-information.cpp
+++ user-information.cpp
@@ -1,3 +1,16 @@
+/**
+*
+* Solution to course project # 9
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2022 / 2023
+*
+*@author Viktoria Koleva
+*@idnumber 5MI0600139 * @compiler VC
+*
+*<file with user-information default values and initialization>
+*
+*/
 #include "user-information.h"
 
 UserInfo::UserInfo() {
